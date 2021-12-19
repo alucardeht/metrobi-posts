4a09e19b3ebf8ba8d1ea4fd85df000e84fe4152a
Julio Lena <juliolena@Julios-MacBook-Pro.local>
2021-12-18 21:22:35 -0400

    Created a Loader component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { LoaderContainer, Roller } from "./styles";
+
+const Loader = () => {
+  return (
+    <LoaderContainer>
+      <Roller>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </Roller>
+    </LoaderContainer>
+  );
+};
+
+export default Loader;
