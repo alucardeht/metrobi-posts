15f1fa27a1bf9cfee473fe87fcc0fdd8f4f20a54
Julio Lena <juliolena@Julios-MacBook-Pro.local>
2021-12-19 11:18:35 -0400

    Fixing a little detail on post screen

--- src/pages/Post/styles.ts
+++ src/pages/Post/styles.ts
@@ -8,6 +8,10 @@ export const PostCardContainer = styled.div`
   box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%),
     0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%);
   margin-top: 2rem;
+
+  @media (max-width: 600px) {
+    width: 20rem;
+  }
 `;
 
 export const PostCardTitle = styled.div`
